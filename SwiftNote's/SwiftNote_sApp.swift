294a4378aa269dcf443c49073a89f63a4964614c
SnakCat <snakkdss@gmail.com>
2026-03-03 21:49:59 +0300

    initial commit

---
+++ SwiftNote's/SwiftNote_sApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNote_sApp.swift
+//  SwiftNote's
+//
+//  Created by SnakCat on 03.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNote_sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
